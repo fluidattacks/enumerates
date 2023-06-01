11a967981b34e94eed7dc5977c49462aa1850daf
Andres Cuberos <acuberos@fluidattacks.com>
2023-06-01 12:50:49 -0500

    refac(job): change pipeline upload label
    - Make the label more clear.
    - Test whether the dynamic logic is working.
    - Use the YAML file instead of STDIN to upload dynamic steps.
    
    Signed-off-by: Andres Cuberos <acuberos@fluidattacks.com>

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -2,10 +2,12 @@
 
 set -eou pipefail
 
+echo "steps:"
+
 while read -r changed_file;
 do
   if [[ "${changed_file}" == src/enumerator.ts ]];
   then
-    cat .buildkite/upload_script.yml
+    buildkite-agent pipeline upload .buildkite/upload_script.yml
   fi
 done < <(git diff HEAD~1 --name-only)
